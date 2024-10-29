d23a5996302a09f4ba7848531bb55d9c7f662241
Shamilakhan936 <shamilak936@gmail.com>
2024-10-29 20:26:30 +0500

    changes

--- src/components/businessResults/BusinessResults.jsx
+++ src/components/businessResults/BusinessResults.jsx
@@ -5,7 +5,7 @@ import BusinessSlider from "../businessSlider/BusinessSlider";
 const BusinessResults = () => {
   return (
     <>
-      <div className="py-[80px]">
+      <div className="py-[80px] max-sm:pl[16px] max-md:pl[57px] lg:px-[150px]">
         <div className="flex flex-col items-center gap-3">
           <h1 className="text-4xl font-semibold text-center">
             Trusted by providers, proven by results
@@ -13,14 +13,14 @@ const BusinessResults = () => {
           <div className="w-[100px] border border-black h-[2px]"></div>
         </div>
 
-        <div className="pt-[53px] md:pl-[150px] flex flex-col md:flex-row items-center">
+        <div className="pt-[53px] flex flex-col items-center md:items-start md:flex-row">
           <img
             src={person}
             alt=""
-            className="w-[75%] md:w-[330px] h-auto flex-shrink-0 mb-6 md:mb-0"
+            className="h-auto flex-shrink-0 mb-6 md:mb-0"
           />
 
-          <div className="slider flex-grow min-w-0 md:ml-4 w-full md:w-auto">
+          <div className="slider flex-grow min-w-0 lg:ml-[121px] ml-[20px] w-full md:w-auto">
             <BusinessSlider />
           </div>
         </div>
