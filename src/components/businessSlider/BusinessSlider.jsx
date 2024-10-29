66be43e6a7a2ef499f4e3ca2c4f998a6104157fa
Shamilakhan936 <shamilak936@gmail.com>
2024-10-30 02:01:40 +0500

    changes

--- src/components/businessSlider/BusinessSlider.jsx
+++ src/components/businessSlider/BusinessSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -11,6 +11,13 @@ import { IoArrowBack, IoArrowForward } from "react-icons/io5";
 const BusinessSlider = () => {
   const prevRef = useRef(null);
   const nextRef = useRef(null);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleSlideChange = (swiper) => {
+    setCurrentIndex(swiper.activeIndex);
+  };
+
   return (
     <div className="relative w-full">
       {/* Swiper */}
@@ -24,6 +31,7 @@ const BusinessSlider = () => {
           swiper.params.navigation.prevEl = prevRef.current;
           swiper.params.navigation.nextEl = nextRef.current;
         }}
+        onSlideChange={handleSlideChange}
         pagination={true}
         mousewheel={true}
         keyboard={true}
@@ -31,13 +39,13 @@ const BusinessSlider = () => {
         className="w-[98%] mx-0 font-poppins"
       >
         {/* First Slide */}
-        <SwiperSlide className="text-start flex flex-col items-start justify-center md:justify-start md:pt-[81px]">
+        <SwiperSlide className="text-start flex flex-col items-start justify-center md:justify-start md:pt-[81px] max-sm:px-1 max-sm:text-center">
           <h2 className="text-[19px] font-semibold text-wrap">
             We have been able to reach more families in need of quality home
             care through Zeegna network. It's made connecting with clients
             simple and has been a key part of our growth.
           </h2>
-          <div className="flex flex-col pt-[40px] gap-[3px] pb-[56px]">
+          <div className="w-full max-sm:text-center flex flex-col pt-[40px] gap-[3px] pb-[56px]">
             <p className="text-[#5E5E6F] text-[17px] font-semibold">Emily S, </p>
             <p className="text-[17px] font-semibold">Home Care Agency Owner</p>
           </div>
@@ -70,18 +78,28 @@ const BusinessSlider = () => {
         </SwiperSlide>
       </Swiper>
       {/* buttons */}
-      <div
-        ref={prevRef}
-        className=" swiper-button-back absolute left-0 top-[100%] transform -translate-y-1/2 z-10 cursor-pointer p-3 border border-black rounded-full flex items-center justify-center"
-      >
-        <IoArrowBack size={24} />
-      </div>
-      <div
-        ref={nextRef}
-        className=" swiper-button-back absolute right-0 top-[100%] transform -translate-y-1/2 z-10 cursor-pointer p-3 border border-black rounded-full flex items-center justify-center"
-      >
-        <IoArrowForward size={24} />
-      </div>
+          <div
+            ref={prevRef}
+            className={` swiper-button-back absolute left-0 top-[100%] transform -translate-y-1/2 z-10 cursor-pointer p-3 rounded-full flex items-center justify-center ${
+              currentIndex === 0
+                ? "border border-[#A7A7A7] text-[#A7A7A7] cursor-default"
+                : "border border-black"
+            }`}
+            onClick={currentIndex === 0 ? null : () => prevRef.current.click()}
+          >
+            <IoArrowBack size={22} />
+          </div>
+          <div
+            ref={nextRef}
+            className={` swiper-button-back absolute right-0 top-[100%] transform -translate-y-1/2 z-10 cursor-pointer p-3 rounded-full flex items-center justify-center ${
+              currentIndex === 2
+                ? "border border-[#A7A7A7] text-[#A7A7A7] cursor-default"
+                : "border border-black"
+            }`}
+            onClick={currentIndex === 2 ? null : () => nextRef.current.click()}
+          >
+            <IoArrowForward size={22} />
+          </div>
     </div>
   );
 };
