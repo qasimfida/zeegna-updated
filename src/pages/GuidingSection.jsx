cc10a3cd8860ad435a6107f5ee30a043388be00b
qasimfida <qasimfida0054@gmail.com>
2024-11-12 14:28:09 -1200

    Guiding principle pagew

--- src/pages/GuidingSection.jsx
+++ src/pages/GuidingSection.jsx
@@ -8,72 +8,47 @@ import GuidingPlatform from "../components/guidingPageCompo/GuidingPlatform";
 function GuidingHeroSection() {
   return (
     <>
-      <div className="bg-gradient-to-t from-lime-200 to-yellow-200 poppin  ">
-        <div className="flex justify-start w-full px-5 py-2 text-gray-500 text-sm font-medium">
-          <Link
-            to="/"
-            className={`hover:underline ${
-              location.pathname === "/" ? "text-gray-500" : "text-black"
-            }`}
-          >
+      <div className=" poppin  relative max-sm:overflow-hidden pb-[50px] h-[700px] max-sm:h-[720px] bg-gradient-to-b from-[#FFF9C6]  via-[#FFF9C6] to-[#F3E677]  poppin  ">
+        <div class="flex justify-start pt-[16px] lg:pt-[30px] pl-[38px] max-sm:pl-[16px] mb-[30px] lg:mb-[37px] w-full text-sm font-medium">
+          <a class="hover:underline text-black" href="/">
             Home
-          </Link>
-          <span className="mx-2">|</span>
-          <Link
-            to="/help-center"
-            className={`hover:underline ${
-              location.pathname === "/guiding-section"
-                ? "text-gray-500"
-                : "text-black"
-            }`}
-          >
+          </a>
+          <span class="mx-2">|</span>
+          <a class="hover:underline text-gray-500" href="/become-partner">
             Guiding Principles
-          </Link>
+          </a>
         </div>
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative flex flex-1 w-full flex-col items-center justify-center text-center  xl:h-screen">
-          <h1 class="max-w-6xl font-display  max-sm:text-3xl  md:text-5xl xl:text-6xl font-semibold tracking-normal	z-10	">
-            Elevating aging,
-            <div class="relative whitespace-nowrap sm:text-3xl md:text-5xl xl:text-6xl ">
-              one family at a time
-            </div>
+        <div class="mx-auto max-sm:mx-[16px]  lg:mt-[60px] flex flex-1 lg:w-full flex-col items-center justify-center text-center">
+          <h1 class="max-w-[685px] font-display  max-sm:text-[28px] lg:text-[64px]  font-semibold 	z-10	">
+            Elevating aging, <br /> one family at a time
           </h1>
-          <h2 class="mx-auto mt-12 max-w-xl text-xl	 max-sm:text-lg  leading-7 z-10">
-            We're inspired by the strength and love of families who choose to
-            support their loved ones age in their own homes. It's that
+          <p class="mx-auto mt-[30px] max-sm:px-[9px] max-sm:mt-[10px] lg:max-w-[775px] text-[20px] text-[#5E5E6F] font-medium	 max-sm:text-[19px]   z-10">
+            At Zeegna, we're inspired by the strength and love of families who
+            choose to support their loved ones age in their own homes. It's that
             resilience that fuels our mission to create something truly
             impactful - a future where aging is not a burden, but a source of
             wisdom, strength, and celebrated way of life
-          </h2>
+          </p>
           <img
             src="images/Ellipse.png"
             alt=""
-            class="ellipse-2 absolute left-[5%] top-[12%]  w-[147px] sm:w-[73px] md:w-[84px] lg:w-[147px] !w-[73px] sm:!w-[73px] md:!w-[84px] lg:!w-[147px] z-0 sm:top-[14%] max-sm:top-[2%] sm:right-[70%]  max-sm:opacity-50"
+            class="ellipse-2 absolute lg:left-[5%] lg:top-[15%] bottom-[181px]  w-[147px] sm:w-[73px] md:w-[84px] lg:w-[147px] !w-[88px] sm:!w-[88px] md:!w-[84px] lg:!w-[147px]   sm:right-[70%]  "
           />
 
           <img
             src="images/Ellipse2.png"
             alt=""
-            class="ellipse-2 absolute left-[77%] top-[11%]  w-[147px] sm:w-[73px] md:w-[84px] lg:w-[147px] !w-[73px] sm:!w-[73px] md:!w-[84px] lg:!w-[147px] z-0  max-sm:opacity-50"
+            class="ellipse-2 absolute lg:right-[7%] lg:top-[10%] bottom-[29px] left-[40%] w-[147px] sm:w-[73px] md:w-[84px] lg:w-[174px] !w-[106px] sm:!w-[106px] md:!w-[84px] lg:!w-[174px] "
           />
           <img
             src="images/Ellipse3.png"
             alt=""
-            class="ellipse-3 absolute left-[2%] top-[60%]  w-[147px] sm:w-[73px] md:w-[84px] lg:w-[147px] !w-[73px] sm:!w-[73px] md:!w-[84px] lg:!w-[147px] z-0 max-sm:right-[77%]  max-sm:opacity-50"
+            class="ellipse-3 absolute lg:left-[5%] lg:bottom-[25%] bottom-[81px]  w-[147px] sm:w-[73px] md:w-[84px] lg:w-[116px] !w-[120px] sm:!w-[120px] md:!w-[84px] lg:!w-[116px]  max-sm:right-[77%] "
           />
           <img
             src="images/Ellipse4.png"
             alt=""
-            class="ellipse-4 absolute right-[13%] top-[68%]  w-[147px] sm:w-[73px] md:w-[84px] lg:w-[147px] !w-[73px] sm:!w-[73px] md:!w-[84px] lg:!w-[147px] z-0 max-sm:left-[77%] max-sm:opacity-50 "
-          />
-          <img
-            src="images/Ellipse5.png"
-            alt=""
-            class="ellipse-5 absolute right-[76%;] top-[91%] max-md-top-[87%]   sm:w-[73px] md:w-[84px]  !w-[73px] sm:!w-[73px] md:!w-[84px]  z-0 max-sm:top-[90%] max-lg-top-[80%]  max-sm:opacity-50 "
-          />
-          <img
-            src="images/Ellipse6.png"
-            alt=""
-            class="ellipse-6 absolute left-[50%] top-[86%]  w-[147px]   lg:w-[147px]  md:!w-[117px] lg:!w-[147px] z-0 sm:!w-[119px] sm:w-[119px]  max-sm:top-[83%]  max-sm:opacity-50  "
+            class="ellipse-4 absolute lg:right-[10%] lg:top-[68%] bottom-[99px]  w-[147px] sm:w-[73px] md:w-[84px] lg:w-[113px] !w-[113px] overflow-hidden sm:!w-[113px] md:!w-[84px] lg:!w-[113px]  max-sm:left-[77%]  "
           />
         </div>
       </div>
