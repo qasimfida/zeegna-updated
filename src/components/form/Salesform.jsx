3a9a24b0beae4217f3b2b47df3feb77909e0b843
qasimfida <qasimfida0054@gmail.com>
2024-11-18 11:51:53 -1200

    worked on guiding principles

--- src/components/form/Salesform.jsx
+++ src/components/form/Salesform.jsx
@@ -4,60 +4,82 @@ import ReactQuill from "react-quill";
 import Uploadicon from "../../assets/icons/contacticons/Uploadicon";
 import "react-quill/dist/quill.snow.css";
 
-const Salesform = () => {
+const Form = () => {
   const [value, setValue] = useState("");
-
   return (
-    <form className="bg-[#F7F7F7] mt-[30px] flex justify-center rounded w-full poppin">
-      <div className="w-[80%] py-12 flex flex-col justify-center">
+    <form className="bg-[#F7F7F7] mt-[30px] flex justify-center rounded-[30px] max-sm:rounded-[15px] w-full poppin">
+      <div className="md:w-[67%] max-sm:mx-[15px] pt-[44px] flex flex-col justify-center">
         <select
-          className="border border-gray-300 rounded-lg bg-transparent px-4 py-3	w-full text-gray-600 text-sm"
-          defaultValue="pleade"
+          className="bg-transparent border border-gray-300 rounded-lg px-4 py-3	w-full text-gray-600 text-sm"
+          defaultValue=""
         >
           <option value="please select a topic">Please select a topic</option>
           <option value="self">one</option>
           <option value="family">Two</option>
           <option value="friend">Three</option>
         </select>
-        <div className="flex flex-col justify-between md:flex-row pt-4 gap-3">
+        <div className="flex flex-col justify-between md:flex-row pt-4 gap-[15px]">
           <FormInput placeholder="Name" />
           <FormInput placeholder="Last Name" />
         </div>
-        <div className="flex flex-col justify-between md:flex-row pt-4 gap-3">
+        <div className="flex flex-col justify-between md:flex-row pt-4 gap-[15px]">
           <FormInput placeholder="Email" />
           <FormInput placeholder="Phone Number" />
         </div>
         <div className="flex flex-col justify-between md:flex-row pt-4 gap-3">
           <FormInput placeholder="Company Name" />
-          <FormInput placeholder="Company Category" />
+          <select
+            className="bg-transparent border border-gray-300 rounded-lg px-4 py-3 md:w-[352px]	 text-gray-600 text-sm"
+            defaultValue=""
+          >
+            <option value="please select a topic">Company category</option>
+            <option value="self">one</option>
+            <option value="family">Two</option>
+            <option value="friend">Three</option>
+          </select>
         </div>
+        ;
         <ReactQuill
           theme="snow"
           value={value}
           onChange={setValue}
           placeholder="Please enter your comments"
-          className="bg-transparent mt-4 rounded-lg border-none"
+          className="bg-transparent mt-4  rounded-t-[15px]	 text-[14px] font-medium"
         />
-        <div className="flex items-center flex-col mt-3 border-dashed rounded-lg border-[2px] border-[#B6B9CE] py-4 gap-2">
+        <div
+          className="flex items-center flex-col mt-3 max-sm:px-[10px] border-dashed rounded-lg border-[2px] border-[#B6B9CE] py-4 gap-[15px] cursor-pointer"
+          onClick={() => document.getElementById("fileUpload").click()}
+        >
           <div>
             <Uploadicon />
           </div>
-          <h3 className="text-base font-semibold text-[#5E5E6F]">
+          <h3 className="text-[17px] font-semibold text-[#5E5E6F]">
             Upload Documents
           </h3>
           <p className="text-[#5E5E6F] text-base">
-            Supports .pdf, .doc, .docx, .ppt, .pptx, .xls, .xlsx types
+            Supports .pdf, .doc, .docx, .ppt, .pptx, .xls, .xlsx types
+          </p>
+
+          <input
+            id="fileUpload"
+            type="file"
+            className="hidden"
+            accept=".pdf,.doc,.docx,.ppt,.pptx,.xls,.xlsx"
+            onChange={(e) => console.log(e.target.files)}
+          />
+        </div>
+        <div className=" flex gap-[15px] mt-[15px] md:items-center">
+          <input
+            type="checkbox"
+            className="!rounded-[5px] w-[22px] !h-[22px]  "
+          />
+          <p className="text-[#5E5E6F] text-sm font-medium">
+            By submitting this form, I acknowledge receipt of the Zeegna. See
+            our Privacy policy
           </p>
         </div>
-        <div className=" flex gap-3 mt-3 items-center">
-          <input type="checkbox" />
-          <p className="text-[#5E5E6F] text-sm">
-            By submitting this form, I acknowledge receipt of the
-            ZyraHealth Privacy policy
-          </p>
-        </div>
-        <div className="flex justify-center mt-10">
-          <button className="py-2 px-14 bg-black hover:bg-transparent hover:text-black hover:border-black border-[1px] text-white rounded-full">
+        <div className="flex justify-center mt-[23px] pb-[56px]">
+          <button className="py-2 px-14 bg-black hover:bg-transparent hover:text-black hover:border-black border-[1px] text-white rounded-full max-sm:w-full">
             Submit
           </button>
         </div>
@@ -66,4 +88,4 @@ const Salesform = () => {
   );
 };
 
-export default Salesform;
+export default Form;
