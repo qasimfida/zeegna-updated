7dc114aae89704d1d84504aa4a13612e62027afd
qasimfida <qasimfida0054@gmail.com>
2024-11-18 11:54:58 -1200

    worked on guiding principles

--- src/components/getintouch/Gethelp.jsx
+++ src/components/getintouch/Gethelp.jsx
@@ -6,7 +6,7 @@ const Gethelp = () => {
   return (
     <>
       <section className="sm:mx-8 max-sm:mx-[16px]  poppin mb-[80px] max-sm:mb-[50px]">
-        <div className="flex-col flex sm:flex-row justify-between gap-4 bg-[#e6fff0] sm:px-20 md:pl-2 rounded-[40px] py-16 max-sm:py-[50px]">
+        <div className="flex-col flex sm:flex-row justify-between gap-4 bg-[#e6fff0] sm:px-20  rounded-[40px] py-16 max-sm:py-[50px]">
           <div className="sm:w-[45%] w-auto flex flex-col items-start max-sm:items-center justify-center gap-[10px] max-sm:text-center">
             <h1 className="text-[32px] font-semibold max-sm:text-[24px] max-sm:text-center">
               Get help now{" "}
