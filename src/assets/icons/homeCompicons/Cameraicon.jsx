f65b833d301e759e6073d784073dfa5df9c1327e
qasimfida <qasimfida0054@gmail.com>
2024-12-03 05:49:45 -1200

    ms two new pages added

---
+++ src/assets/icons/homeCompicons/Cameraicon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cameraicon = () => {
+  return (
+      <svg width="30" height="18" viewBox="0 0 30 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M26.2 1H3.8C2.2536 1 1 2.19391 1 3.66667V14.3333C1 15.8061 2.2536 17 3.8 17H26.2C27.7464 17 29 15.8061 29 14.3333V3.66667C29 2.19391 27.7464 1 26.2 1Z" stroke="#5FB471" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M15 12C16.6569 12 18 10.6569 18 9C18 7.34315 16.6569 6 15 6C13.3431 6 12 7.34315 12 9C12 10.6569 13.3431 12 15 12Z" stroke="#5FB471" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M7 9H7.01416M23.9858 9H24" stroke="#5FB471" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+  )
+}
+
+export default Cameraicon
---
+++ src/assets/icons/homeCompicons/FileIcon.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FileIcon = () => {
+  return (
+    <div>
+    <svg width="22" height="28" viewBox="0 0 22 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M14.75 1.5H3.5C2.83696 1.5 2.20107 1.76339 1.73223 2.23223C1.26339 2.70107 1 3.33696 1 4V24C1 24.663 1.26339 25.2989 1.73223 25.7678C2.20107 26.2366 2.83696 26.5 3.5 26.5H18.5C19.163 26.5 19.7989 26.2366 20.2678 25.7678C20.7366 25.2989 21 24.663 21 24V7.75L14.75 1.5Z" stroke="#A08ACA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M13.5 1.5V6.5C13.5 7.16304 13.7634 7.79893 14.2322 8.26777C14.7011 8.73661 15.337 9 16 9H21" stroke="#A08ACA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M8.5 10.25H6" stroke="#A08ACA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M16 15.25H6" stroke="#A08ACA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M16 20.25H6" stroke="#A08ACA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    </div>
+  )
+}
+
+export default FileIcon
